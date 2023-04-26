f37e2136609c03c5af2f0835e2d7573666d7ffa6
arthurszef <arthurszef@gmail.com>
2023-04-26 22:32:31 +0200

    ajout de FilmController, newFilm et index Film

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -6,11 +6,15 @@ use App\Entity\Salle;
 use App\Entity\Role;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 
 class ProfilController extends AbstractController
 {
+
+    private $entityManager;
+
     #[Route('/profil', name: 'app_profil')]
     public function index(EntityManagerInterface $entityManager): Response
     {
