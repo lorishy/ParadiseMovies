b7b40de166eea6f77eb34d5dee5a512da0a84ded
arthurszef <arthurszef@gmail.com>
2023-05-24 10:32:08 +0200

    maj vers symfony 6.2 et ajout knpPaginator bundle

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -4,13 +4,21 @@ namespace App\Controller;
 
 use App\Entity\Categorie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CategorieRepository;
 use App\Form\CategorieType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategorieController extends AbstractController
 {
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
     #[Route('/vod/categorie', name: 'app_categorie')]
     public function index(): Response
     {
@@ -20,24 +28,38 @@ class CategorieController extends AbstractController
     }
 
 
-    #[Route('/vod/categorie/ajouter', name: 'categories_add')]
-    public function ajouter(Request $request): Response
+    #[Route('/categorie/ajouter', name: 'categories_add')]
+    public function ajouter(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        
         $categorie = new Categorie();
-        $form = $this->createForm(CategorieType::class, $catégorie);
+        $form = $this->createForm(CategorieType::class, $categorie);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($categorie);
-            $entityManager->flush();
 
-            $this->addFlash('success', 'La catégorie a été ajoutée avec succès.');
 
-            return $this->redirectToRoute('films_index');
+            $libelle = $categorie->getLibelle();
+            $categorie->setLibelle(ucfirst($libelle)); // Convertir la première lettre en majuscule
+    
+            $existingCategorie = $this->entityManager->getRepository(Categorie::class)->findOneBy([
+                'libelle' => $categorie->getLibelle(),
+            ]);
+    
+            if ($existingCategorie) {
+                $this->addFlash('error', 'La catégorie existe déjà.');
+            } else {
+                $this->entityManager->persist($categorie);
+                $this->entityManager->flush();
+    
+                $this->addFlash('success', 'La catégorie a été ajouté avec succès.');
+            }
+
+            // return $this->redirectToRoute('films_index');
         }
 
-        return $this->render('vod/categories/ajouter.html.twig', [
+        return $this->render('vod/categories/add.html.twig', [
             'form' => $form->createView(),
         ]);
     }
